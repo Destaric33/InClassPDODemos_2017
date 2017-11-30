900a66ee77196940ba0b8c444043a9404ff2a869
malla <malla@172.28.0.77>
2017-11-30 09:21:08 -0400

    november 30th

---
+++ addcategory.php
@@ -0,0 +1,7 @@
+<!-- Add Category from Vendors/Landing page -->
+
+<?php
+
+include 'includes/header.php';//top header
+    include 'templates/addcategory.php'; //article content
+    include 'includes/footer.php';//bottom footer
