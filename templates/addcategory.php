457616eab0dc459bef9dd6e1d2b30f83d6d17d41
malla <malla@172.28.0.77>
2017-11-30 09:20:38 -0400

    Added alot of love

---
+++ templates/addcategory.php
@@ -0,0 +1,78 @@
+<!-- Page Content -->
+<div class="container">
+    <h1 class="mt-4 mb-3">Add New Category</h1>
+
+    <!-- mwilliams:  breadcrumb navigation -->
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+        <li class="breadcrumb-item active">Add Category</li>            
+    </ol>
+    <!-- end breadcrumb -->
+
+
+
+
+    <?php
+    //1. Check if form was submitted
+    if($_SERVER['REQUEST_METHOD']== 'POST'){
+        //var_dump($_POST);
+ $category = trim(filter_var($_POST['category'],FILTER_SANITIZE_STRING) );
+  //Test if user actually entered something
+ if(!empty($category)){
+     
+         require './includes/config.php';
+        require MYSQL;
+        
+        //build our INSERT statment - using prepared statement
+        $stmt = $dbc->prepare("INSERT INTO categories(category)
+                               VALUES (:category)");
+        
+        //Bind our named parameter :category to user input value
+        $stmt->bindValue(':category',$category,PDO::PARAM_STR);
+        
+        try{
+            //try to execute the query
+            $stmt->execute();
+            echo "<div class='alert alert-success' role='alert'>
+            The category <strong>$category</strong> has been inserted!!<br>
+                <a href=''>Add Another</a>
+            </div>";
+        } catch (Exception $ex) {
+            $code = $ex->getcode();
+            $message = 'Unknown system error!';
+            if ($code == 23000){
+                $message = 'You cannot insert a duplicate category!';
+            }
+            //If an error occurs, it will be trapped here
+            echo "<div class='alert alert-danger' role='alert'>
+            The category <strong>$category</strong> was not inserted
+            due to a system error!<br>".
+            $Message().
+             "<p><a href=''>Please try again</a></p>
+            </div>";
+        }//END of try catch block
+        
+ }//end if not empty
+ 
+    }else{
+
+    
+   
+    ?>
+
+    <form class="form-inline" method="post" action="addcategory.php">
+        <div class="form-group mx-sm-3">
+            <label for="category" class="sr-only">Category</label>
+            <input type="text" class="form-control" 
+                   id="category" name="category" 
+                   placeholder="Enter the category">
+        </div>
+        <button type="submit" class="btn btn-primary">Add Category</button>
+    </form>
+
+<?php
+
+    }//If 
+    
+    ?>
+</div>
